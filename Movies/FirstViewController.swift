44998dfe7434747c60788187eb8e7b316907a710
timroesner <timroesner@me.com>
2017-02-13 17:49:52 -0800

    Initial Commit

---
+++ Movies/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Movies
+//
+//  Created by Tim Roesner on 2/13/17.
+//  Copyright © 2017 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
